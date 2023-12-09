056e9fe70133f7f9f4c39e0ea108053c216aced2
azraa2005 <azragokhan84@gmail.com>
2023-12-09 14:31:40 +0100

    imported footer

--- footer.js
+++ footer.js
@@ -1,2 +1,10 @@
 export const footer=
-``
+`  <div class="card text-center fixed-bottom bg-dark-subtle p-3">
+<div class="card-body">
+  <h5 class="card-title">Hicoders 2023/2024 LMS </h5>
+  <p class="card-text"> This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+</div>
+<div class="card-footer text-body-secondary">
+ About Us | Contact
+</div>
+</div>`
--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-import {teachersData} from "./teachers.js"
 // import {students} from "./students.js"
 import {header} from "./header.js"
 import{render} from "./render.js"
-
-render ("#script",header,teachersData)
+import {footer} from "./footer.js"
+import {teachersData} from "./teachers.js"
+render ("#script",header,teachersData,footer)
 
 
 document.querySelector("#home-link").addEventListener("click",()=> {render("#content",content)}) 
