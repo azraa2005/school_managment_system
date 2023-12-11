786bea2e7dcd44af90da7c54eada1af2513aa805
azraa2005 <azragokhan84@gmail.com>
2023-12-11 22:30:35 +0100

    added the navbar and the footer to the index.html

--- footer.js
+++ footer.js
@@ -1,10 +1,9 @@
-export const footer=
-`  <div class="card text-center fixed-bottom bg-dark-subtle p-3">
-<div class="card-body">
-  <h5 class="card-title">Hicoders 2023/2024 LMS </h5>
-  <p class="card-text"> This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-</div>
-<div class="card-footer text-body-secondary">
- About Us | Contact
-</div>
-</div>`
+export const footer=`
+<footer class="bg-body-tertiary text-center text-lg-start">
+  <!-- Copyright -->
+  <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.05);">
+    © 2020 Copyright:
+    <a class="text-body" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+  </div>
+  <!-- Copyright -->
+</footer>`
